ad3081e5a2407b83d4ea08536cfa3485041ea0aa
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2024-02-28 16:13:31 +0200

    Pass down the state and state changing functions

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import hero from '../assets/icons_assets/restauranfood.jpg';
+import Bookings from './Bookings';
 
-function Hero() {
+function Hero(props) {
   return (
     <section className='hero'>
-      <div className='hero-section'>
-        <h1 className='hero-title'>Little Lemon</h1>
-        <h3 className='location'>Chicago</h3>
-        <div className='hero-description d-flex'>
-          <p className='description Karla f-medium clr-white'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a moder twist.</p>
-          <img className='img-hero img' src={hero} alt='Food image'/>
-        </div>
-        <button className='bton'><NavLink to='/reservation'>Reserve a table</NavLink></button>
-      </div>
+        {props.reservePressed ? <Bookings updateDate={props.updateDate} updateDiners={props.updateDiners} updateOccasion={props.updateOccasion}
+        updateSeat={props.updateSeat} updateTime={props.updateTime} resdate={props.resdate} availableTimes={props.availableTimes}
+        resdiners={props.resdiners} resoccasion={props.resoccasion} resseat={props.resseat} submitForm={props.submitForm} formData={props.formData} onSubmit={props.onSubmit} reservePressed={props.reservePressed}/> : <>
+          <div className='hero-section'>
+            <h1 className='hero-title'>Little Lemon</h1>
+            <h3 className='location'>Chicago</h3>
+            <div className='hero-description d-flex'>
+              <p className='description Karla f-medium clr-white'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a moder twist.</p>
+              <img className='img-hero img' src={hero} alt='Food image'/>
+            </div>
+            <button className='bton' onClick={props.showBookings}>Reserve a table</button>
+          </div>
+        </>}
     </section>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,10 +5,12 @@ import CustomersSay from '../components/CustomersSay';
 import Chicago from '../components/Chicago';
 import './homePage.css';
 
-function Main() {
+function Main(props) {
   return (
     <main>
-      <Hero/>
+      <Hero updateDate={props.updateDate} updateDiners={props.updateDiners} updateOccasion={props.updateOccasion}
+        updateSeat={props.updateSeat} updateTime={props.updateTime} resdate={props.resdate} availableTimes={props.availableTimes}
+        resdiners={props.resdiners} resoccasion={props.resoccasion} resseat={props.resseat} submitForm={props.submitForm} formData={props.formData} onSubmit={props.onSubmit} reservePressed={props.reservePressed} showBookings={props.showBookings}/>
       <Highlights/>
       <CustomersSay/>
       <Chicago/>
