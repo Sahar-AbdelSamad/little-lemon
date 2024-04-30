f0c7e216fb8cc796ae59870194af02dd1ef5beaa
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2024-04-30 07:54:20 +0300

    Fix Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,14 @@
-import React ,{useState} from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Nav from './Nav';
 import './header_footer.css';
-import { NavLink } from 'react-router-dom';
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  }
   return (
     <header className='header screen-width'>
-      <button className='hamburgerbtn' onClick={handleToggle}></button>
-      {isOpen ? <div className='mobile-nav'><Nav isOpen={isOpen} handleToggle={handleToggle}/></div> : ''}
+      <Nav />
       <NavLink to='/'><div className='logo'/></NavLink>
       <button className='basket'></button>
-      <div className='navbar'><Nav/></div>
     </header>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { NavLink } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi';
+import { MdClose } from 'react-icons/md';
 
-function Nav({isOpen, handleToggle}) {
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  }
   const links = [
     {
       id:1,
@@ -36,10 +42,11 @@ function Nav({isOpen, handleToggle}) {
   ]
   return (
     <nav className='navigation'>
-      {isOpen ? <img onClick={handleToggle} className='closebtn' src={require('../assets/icons_assets/closeicon.png')} alt='close icon'/> : ''}
-      <ul>
+      <button className='hamburgerbtn' onClick={handleToggle}>
+      {!isOpen ? <FiMenu /> : <div className='closebtn'><MdClose /></div>}</button>
+      <ul className={`menu-nav ${isOpen ? 'show-menu' : '' }`}>
         {links.map((link=>(
-          <li key={link.id}><NavLink to={link.path} onClick={handleToggle}>{link.text}</NavLink></li>
+          <li key={link.id}><NavLink to={link.path} onClick={() => setIsOpen(false)}>{link.text}</NavLink></li>
         )))}
       </ul>
     </nav>
