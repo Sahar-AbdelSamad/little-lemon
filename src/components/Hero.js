e2721d1911672ef09d523cfe07dc8ac1b213dd76
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2024-05-07 12:55:42 +0300

    Fix Hero's style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero(props) {
     <section className='hero'>
       <div className='hero-section'>
         {props.reservePressed ? <Bookings setBookingConfirmed={props.setBookingConfirmed} reservePressed={props.reservePressed} showBookings={props.showBookings} bookingConfirmed={props.bookingConfirmed} formData={props.formData} submitForm={props.submitForm}/> : <>
-            <h1 className='hero-title'>Little Lemon</h1>
+            <h1 className='hero-title pt-md-3'>Little Lemon</h1>
             <h3 className='location'>Chicago</h3>
             <div className='hero-description d-flex'>
               <p className='description Karla f-medium clr-white'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a moder twist.</p>
